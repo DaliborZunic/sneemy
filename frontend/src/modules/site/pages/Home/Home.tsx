46bffe7a2457f8273b346afb584f911938af4d63
Dalibor Žunić <dalibor.zunic2@gmail.com>
2025-10-27 20:05:43 +0100

    Start implementing the layout with one background div

--- frontend/src/modules/site/pages/Home/Home.tsx
+++ frontend/src/modules/site/pages/Home/Home.tsx
@@ -1,8 +1,12 @@
+import Hero from "./components/Hero/Hero";
+import "./Home.css";
+
 export default function Home() {
   return (
-    <section>
-      <h1>Welcome to the Site Home</h1>
-      <p>This is your public landing page. Start applying your design here.</p>
-    </section>
+    <>
+    <div className="home-wrapper">
+      <Hero />
+    </div>
+    </>
   );
 }
---
+++ frontend/src/modules/site/pages/Home/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero-wrapper">
+        
+    </div>
+  )
+}
+
+export default Hero
