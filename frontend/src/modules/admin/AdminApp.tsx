08bb1b09e0a89e7cdc2a46ae5902ea3568cafd75
Dalibor Žunić <dalibor.zunic2@gmail.com>
2025-10-27 14:37:57 +0100

    Add individual css for site and admin

--- frontend/src/modules/admin/AdminApp.tsx
+++ frontend/src/modules/admin/AdminApp.tsx
@@ -1,3 +1,4 @@
+import "./AdminApp.css";
 import { Routes, Route, Link } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
--- frontend/src/modules/site/SiteApp.tsx
+++ frontend/src/modules/site/SiteApp.tsx
@@ -1,3 +1,4 @@
+import "./SiteApp.css";
 import { Routes, Route, Link } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
