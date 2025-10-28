5daba2ae115dbdb82d7bbb3e8fb98ff5b134c2f4
Dalibor Žunić <dalibor.zunic2@gmail.com>
2025-10-28 20:34:26 +0100

    Start working on Services section

---
+++ frontend/src/modules/site/pages/Home/components/Services/Services.tsx
@@ -0,0 +1,16 @@
+import "./Services.css";
+
+const Services = () => {
+    return (
+        <div className="services-section">
+            <div className="section-content-wrapper">
+                <h1 className="services-heading">Profesionalan i pristupačan video sadržaj
+                    bez studijskih procesa i troškova</h1>
+                <span className="services-lead">Pošaljite nam svoj logo i par riječi o svom obrtu, OPG-u i opišite nam kako ulažete svakodnevnu strast i energiju. Naš team kreativaca će osmisliti scenarij i video pomoću kojeg ćete komunicirati sa svojim klijentima kao nikad do sad.</span>
+            </div>
+        </div>
+
+    )
+}
+
+export default Services
