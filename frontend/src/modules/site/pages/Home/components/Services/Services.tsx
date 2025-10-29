5abc93070baad1e3e8d6cc03e77a92022f3850cd
Dalibor Žunić <dalibor.zunic2@gmail.com>
2025-10-29 11:45:55 +0100

    Add youtube service

--- frontend/src/modules/site/pages/Home/components/Services/Services.tsx
+++ frontend/src/modules/site/pages/Home/components/Services/Services.tsx
@@ -1,12 +1,24 @@
 import "./Services.css";
+import laptopFrame from "../../../../../../assets/laptop-frame.svg";
+import youtubeLogoColor from "../../../../../../assets/youtube-logo-color.svg";
 
 const Services = () => {
     return (
         <div className="services-section">
             <div className="section-content-wrapper">
-                <h1 className="services-heading">Profesionalan i pristupačan video sadržaj
-                    bez studijskih procesa i troškova</h1>
+                <h2 className="services-heading">Profesionalan i pristupačan video sadržaj
+                    bez studijskih procesa i troškova</h2>
                 <span className="services-lead">Pošaljite nam svoj logo i par riječi o svom obrtu, OPG-u i opišite nam kako ulažete svakodnevnu strast i energiju. Naš team kreativaca će osmisliti scenarij i video pomoću kojeg ćete komunicirati sa svojim klijentima kao nikad do sad.</span>
+                <div className="youtube-service-wrapper">
+                    <div className="text-column">
+                        <img className="youtube-service-logo" src={youtubeLogoColor} alt="youtube" />
+                        <p className="services-text">Pošaljite nam svoj logo i par riječi o svom obrtu, OPG-u i opišite nam kako ulažete svakodnevnu strast i energiju. Naš team kreativaca će osmisliti scenarij i video pomoću kojeg ćete komunicirati sa svojim klijentima kao nikad do sad.</p>
+                        <p className="services-text">Naš team kreativaca će osmisliti scenarij i video pomoću kojeg ćete komunicirati sa svojim klijentima kao nikad do sad.</p>
+                    </div>
+                    <div className="videos-column">
+                        <img src={laptopFrame} alt="facebook" />
+                    </div>
+                </div>
             </div>
         </div>
 
