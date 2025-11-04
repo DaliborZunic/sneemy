c73bd12d657786be323e737ae6ae23b5ac1bf067
Dalibor Žunić <dalibor.zunic2@gmail.com>
2025-11-04 18:22:04 +0100

    Add moblie navigation

--- frontend/src/modules/site/components/MainNavigation/MainNavigation.tsx
+++ frontend/src/modules/site/components/MainNavigation/MainNavigation.tsx
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom";
 import "./MainNavigation.css";
 import SneemyLogo from "../shared/SneemyLogo/SneemyLogo";
+import hamburgerIcon from "../../../../assets/hamburger-icon.svg"
+import closeIcon from "../../../../assets/close-icon.svg"
+import { useState } from "react";
 
 const MainNavigation = () => {
+    const [menuShown, setMenuShown] = useState<boolean>(false);
+
+    const handleHamburgerClick = () => {
+        setMenuShown(prev => !prev)
+    }
+
     return (
         <div className="main-navigation-wrapper">
             <SneemyLogo logoColorHex="#FBFE00" />
-            <nav className="main-navigation">
+            <nav className={`main-navigation ${menuShown ? "menu-shown" : ""}`}>
                 <Link to="/">Home</Link>
                 <Link to="/about-us">About Us</Link>
                 <Link className="highlighted-nav-link" to="/">Naruči video</Link>
             </nav>
+            <div className="hamburger-icon-wrapper" onClick={handleHamburgerClick}>
+                { !menuShown ? <img className="hamburger-icon" src={hamburgerIcon} alt="hamburger icon" /> : menuShown && <img className="hamburger-icon" src={closeIcon} alt="close icon" /> }
+            </div>
         </div>
-
     )
 }
 
