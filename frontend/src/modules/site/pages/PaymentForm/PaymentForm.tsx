251a880a3a2b879ec84beb2967167b50a861ef8f
Dalibor Žunić <dalibor.zunic2@gmail.com>
2025-11-07 19:37:22 +0100

    Add checkbox

--- frontend/src/modules/site/pages/PaymentForm/PaymentForm.tsx
+++ frontend/src/modules/site/pages/PaymentForm/PaymentForm.tsx
@@ -1,6 +1,37 @@
 import "./PaymentForm.css"
+import checkboxUncheckedIcon from "../../../../assets/checkbox-unchecked.svg"
+import checkboxCheckedIcon from "../../../../assets/checkbox-checked.svg"
+import { useState } from "react";
 
-export default function About() {
+export default function PaymentForm() {
+  const [formData, setFormData] = useState({
+    nameAndLastName: "",
+    eMail: "",
+    phoneNumber: "",
+    website: "",
+    customerRequest: "",
+    isR1Reciept: false,
+    companyName: "",
+    companyOIB: ""
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const toggleR1 = () => {
+    console.log("clicked");
+    
+    setFormData(prev => ({ ...prev, isR1Reciept: !prev.isR1Reciept }));
+  };
+
+  const onPayButton = () => {
+    console.log("formData", formData);
+  };
+
   return (
     <section className="payment-form-section">
       <div className="section-content-wrapper">
@@ -8,37 +39,100 @@ export default function About() {
         <div className="payment-form">
           <div className="form-row">
             <div className="form-input-item">
-              <input type="text" placeholder="ime i prezime*" />
+              <input
+                type="text"
+                name="nameAndLastName"
+                placeholder="ime i prezime*"
+                value={formData.nameAndLastName}
+                onChange={handleChange}
+              />
             </div>
             <div className="form-input-item">
-              <input type="text" placeholder="e-mail*" />
+              <input
+                type="email"
+                name="eMail"
+                placeholder="e-mail*"
+                value={formData.eMail}
+                onChange={handleChange}
+              />
             </div>
           </div>
+
           <div className="form-row">
             <div className="form-input-item">
-              <input type="text" placeholder="broj telefona" />
+              <input
+                type="text"
+                name="phoneNumber"
+                placeholder="broj telefona"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+              />
             </div>
             <div className="form-input-item">
-              <input type="text" placeholder="web stranica" />
+              <input
+                type="text"
+                name="website"
+                placeholder="web stranica"
+                value={formData.website}
+                onChange={handleChange}
+              />
             </div>
           </div>
+
           <div className="form-row">
-            <textarea className="order-description" name="" id="" placeholder="opišite nam tko ste i što nudite svojim klijentima"></textarea>
+            <textarea
+              className="order-description"
+              name="customerRequest"
+              placeholder="opišite nam tko ste i što nudite svojim klijentima"
+              value={formData.customerRequest}
+              onChange={handleChange}
+            />
           </div>
+
           <div className="form-row">
             <div className="upload-field">
               <span>ovdje povucite svoj logo, fotografije, brošure...</span>
             </div>
           </div>
+
           <div className="form-row">
-            <div className="form-input-item">
-              <input type="text" placeholder="naziv tvrtke, obrta, OPG-a..." />
-            </div>
-            <div className="form-input-item">
-              <input type="text" placeholder="OIB tvrtke, obrta, OPG-a..." />
+            <div className="checkbox-wrapper"
+              onClick={toggleR1}
+              role="button"
+            >
+              <img
+                className="checkbox-icon"
+                src={formData.isR1Reciept ? checkboxCheckedIcon : checkboxUncheckedIcon}
+                alt=""
+              />
+              <span className="checkbox-label">želim R1 račun</span>
             </div>
           </div>
-          <button className="pay-video-button">Plati video</button>
+
+          {formData.isR1Reciept && <div className="form-row">
+            <div className="form-input-item">
+              <input
+                type="text"
+                name="companyName"
+                placeholder="naziv tvrtke, obrta, OPG-a..."
+                value={formData.companyName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-input-item">
+              <input
+                type="text"
+                name="companyOIB"
+                placeholder="OIB tvrtke, obrta, OPG-a..."
+                value={formData.companyOIB}
+                onChange={handleChange}
+              />
+            </div>
+          </div>}
+
+          <button className="pay-video-button" onClick={onPayButton}>
+            Plati video
+          </button>
         </div>
       </div>
     </section>
