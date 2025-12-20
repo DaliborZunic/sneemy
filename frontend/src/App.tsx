89c9bc8719ed3bf2db12d7d2c469a6d17ce90a0e
Dalibor Žunić <dalibor.zunic2@gmail.com>
2025-12-20 12:58:29 +0100

    Scroll website to top on route change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import { AuthProvider } from "./auth/AuthContext";
+import ScrollToTop from "./components/ScrollToTop";
 import Login from "./modules/admin/pages/Login/Login";
 
 const SiteApp = lazy(() => import("./modules/site/SiteApp"));
@@ -9,6 +10,7 @@ const AdminApp = lazy(() => import("./modules/admin/AdminApp"));
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <AuthProvider>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
